bf21af761b287891d2a1de29755de4cce644e36a
unknown <soyinkas1@gmail.com>
2023-10-13 23:29:25 +0100

    data validation pipeline added

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from src.mlProject import logger
 from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+
 
 
 # from mlProject import logger # another way
@@ -13,3 +15,13 @@ except Exception as e:
     logger.exception(e)
     raise e
 
+
+STAGE_NAME = 'Data Validation Stage'
+try:
+    logger.info(f'>>>>>> stage {STAGE_NAME} started <<<<<<')
+    data_validation = DataValidationTrainingPipeline()
+    data_validation.main()
+    logger.info(f'>>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx========x')
+except Exception as e:
+    logger.exception(e)
+    raise e
