32e61476831dfa71656ffeea9e7848cd469e9717
unknown <soyinkas1@gmail.com>
2023-10-18 20:26:26 +0100

    app updated

--- app.py
+++ app.py
@@ -22,7 +22,32 @@ def index():
         try:
             # reading the inputs given by the user
             fixed_acidity = float(request.form['fixed_acidity'])
-            volatile
+            volatile_acidity = float(request.form['volatile_acidity'])
+            citric_acid = float(request.form['citric_acid'])
+            residual_sugar = float(request.form['residual_sugar'])
+            chlorides = float(request.form['chlorides'])
+            free_sulphur_dioxide = float(request.form['free_sulphur_dioxide'])
+            total_sulphur_dioxide = float(request.form['total_sulphur_dioxide'])
+            density = float(request.form['density'])
+            pH = float(request.form['pH'])
+            sulphates = float(request.form['sulphates'])
+            alcohol = float(request.form['alcohol'])
+
+            data = [fixed_acidity, volatile_acidity, citric_acid, residual_sugar, chlorides,
+                    free_sulphur_dioxide, total_sulphur_dioxide, density, pH, sulphates, alcohol]
+            data = np.array(data).reshape(1, 11)
+
+            obj = PredictionPipeline()
+            predict = obj.predict(data)
+
+            return render_template('results.html', prediction=str(predict))
+
+        except Exception as e:
+            print('The Exception message is: ', e)
+            return 'something is wrong'
+
+    else:
+        render_template('index.html')
 
 
 if __name__ == '__main__':
